f2380f5d719fe6b9f1c6adbf419ed18fb5e8025e
Rik Schaaf <schaaf.r@tkppensioen.nl>
2022-12-02 17:29:07 +0100

    Day 2: treat X, Y and Z as result -1, 0, 1 and get shape from result

--- aoc-2022-kotlin-day2/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day2/src/main/kotlin/Main.kt
@@ -6,12 +6,30 @@ fun main() {
         .readText()
         .split("\n")
 
+    // 2a
     var totalScore = 0
     for (line in text) {
         if (line.isEmpty()) break
         val codes = line.split(" ")
         val opponentShape = Shape.fromOpponentCode(codes[0].first())
         val myShape = Shape.fromMyCode(codes[1].first())
+        val roundScore = myShape.getScore(opponentShape)
+        println(roundScore)
+        totalScore += roundScore
+    }
+
+    println("Total: $totalScore")
+
+    println()
+
+    // 2b
+    totalScore = 0
+    for (line in text) {
+        if (line.isEmpty()) break
+        val codes = line.split(" ")
+        val opponentShape = Shape.fromOpponentCode(codes[0].first())
+        val myResult = (codes[1].first() - 'V').toByte()
+        val myShape = opponentShape.getOtherShape(myResult)
         val roundScore = myShape.getScore(opponentShape)
         println(roundScore)
         totalScore += roundScore
--- aoc-2022-kotlin-day2/src/main/kotlin/Shape.kt
+++ aoc-2022-kotlin-day2/src/main/kotlin/Shape.kt
@@ -4,16 +4,28 @@ enum class Shape(val opponentCode: Char, val myCode: Char, val value: Byte) {
     SCISSORS('C', 'Z', 3);
 
     fun getScore(other: Shape): Int {
-        return Math.floorMod(this.value - other.value + 1, 3) * 3 + this.value
+        return Math.floorMod(getResult(other) + 1, 3) * 3 + this.value
+    }
+    fun getResult(other: Shape): Int {
+        return this.value - other.value
+    }
+
+    fun getOtherShape(otherResult: Byte): Shape {
+        val otherValue = Math.floorMod((this.value - 1 + otherResult), 3) + 1
+        return fromValue(otherValue.toByte())
     }
 
     companion object {
+        fun fromOpponentCode(opponentCode: Char): Shape {
+            return values().first { with(it.opponentCode, opponentCode::equals) }
+        }
+
         fun fromMyCode(myCode: Char): Shape {
             return values().first { with(it.myCode, myCode::equals) }
         }
 
-        fun fromOpponentCode(opponentCode: Char): Shape {
-            return values().first { with(it.opponentCode, opponentCode::equals) }
+        fun fromValue(value: Byte): Shape {
+            return values().first { with(it.value, value::equals) }
         }
     }
 }
