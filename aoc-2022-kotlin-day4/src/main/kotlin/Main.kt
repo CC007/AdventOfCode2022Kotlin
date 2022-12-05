9a0044109071a994b706ce1c7900f8eeba28da18
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-05 22:50:22 +0100

    Day 4: Implement overlap count

--- aoc-2022-kotlin-day4/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day4/src/main/kotlin/Main.kt
@@ -9,11 +9,14 @@ fun main() {
         .split("\n")
 
     var fullyContainedCount = 0
+    var overlapCount = 0
     for (line in text) {
         if (line.isEmpty()) break
         val sectionAssignments = line.split(",")
         val sectionRanges = sectionAssignments.map { it.split("-").map(String::toInt) }
         logger.debug(sectionRanges.toString())
+        
+        //4a
         val aContainsB = doesAContainB(sectionRanges[0], sectionRanges[1])
         val bContainsA = doesAContainB(sectionRanges[1], sectionRanges[0])
         logger.debug(aContainsB.toString())
@@ -21,9 +24,18 @@ fun main() {
         if (aContainsB || bContainsA) {
             fullyContainedCount++
         }
+        
+        //4b
+        val aOverlapsB = doesAOverlapB(sectionRanges[0], sectionRanges[1])
+        logger.debug(aOverlapsB.toString())
+        if (aOverlapsB) {
+            overlapCount++
+        }
     }
 
     logger.info("Fully contained count: $fullyContainedCount")
+    logger.info("Overlap count: $overlapCount")
 }
 
 private fun doesAContainB(a: List<Int>, b: List<Int>): Boolean = a[0] <= b[0] && a[1] >= b[1]
+private fun doesAOverlapB(a: List<Int>, b: List<Int>): Boolean = a[0] <= b[1] && a[1] >= b[0]
