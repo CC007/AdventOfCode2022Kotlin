b44c70cf72631dac2cd51adbb072f906c4ef5abf
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-05 01:46:35 +0100

    Day 3: implement intersection, priority and sum for elf groups

--- aoc-2022-kotlin-day3/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day3/src/main/kotlin/Main.kt
@@ -1,27 +1,50 @@
 fun main() {
     val text = object {}.javaClass
-        .getResource("input.txt")!!
+        .getResource("inputExample.txt")!!
         .readText()
         .split("\n")
 
-    // 3a
     var prioritySum = 0
+    var groupPrioritySum = 0
+    var elfNr = 0;
+    val elfGroups = arrayListOf("", "", "")
     for (line in text) {
         if (line.isEmpty()) break
-        val leftCompartment = line.substring(0, line.length/2).asIterable()
-        val rightCompartment = line.substring(line.length/2).asIterable()
-        val intersection = leftCompartment.intersect(rightCompartment).first()
-        // println(intersection)
-        
-        val priority: Int;
-        if (intersection - 'a' >= 0) {
-            priority  = intersection - 'a' + 1
-        } else {
-            priority = intersection - 'A' + 27
-        }
-        // println(priority)
+
+        //3a
+        val leftCompartment = line.substring(0, line.length / 2).toSet()
+        val rightCompartment = line.substring(line.length / 2).toSet()
+        val intersection = leftCompartment.intersect(rightCompartment)
+        println("i: $intersection")
+
+        val priority: Int = getPriority(intersection.first())
+        println("p: $priority")
         prioritySum += priority
+
+        //3b
+        elfGroups[elfNr] = line;
+
+        if (elfNr == 2) {
+            val asIterable = elfGroups.map(CharSequence::toSet)
+            val groupIntersection = asIterable[0].intersect(asIterable[1]).intersect(asIterable[2])
+            println("gi: $groupIntersection")
+            val groupPriority = getPriority(groupIntersection.first())
+            println("gp: $groupPriority")
+            groupPrioritySum += groupPriority
+        }
+        elfNr = (elfNr + 1) % 3;
     }
-    
+
     println("Priority sum: $prioritySum")
+    println("Group priority sum: $groupPrioritySum")
+}
+
+private fun getPriority(intersection: Char): Int {
+    val priority: Int;
+    if (intersection - 'a' >= 0) {
+        priority = intersection - 'a' + 1
+    } else {
+        priority = intersection - 'A' + 27
+    }
+    return priority
 }
