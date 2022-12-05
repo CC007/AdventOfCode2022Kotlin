8af8a78e7bb0dd232ff3b05c65a3fb20a5609a32
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-05 01:53:48 +0100

    Day 3: use kotlin logger instead of println

--- aoc-2022-kotlin-day3/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day3/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main() {
     val text = object {}.javaClass
         .getResource("inputExample.txt")!!
@@ -15,10 +19,10 @@ fun main() {
         val leftCompartment = line.substring(0, line.length / 2).toSet()
         val rightCompartment = line.substring(line.length / 2).toSet()
         val intersection = leftCompartment.intersect(rightCompartment)
-        println("i: $intersection")
+        logger.debug("i: $intersection")
 
         val priority: Int = getPriority(intersection.first())
-        println("p: $priority")
+        logger.debug("p: $priority")
         prioritySum += priority
 
         //3b
@@ -27,16 +31,16 @@ fun main() {
         if (elfNr == 2) {
             val asIterable = elfGroups.map(CharSequence::toSet)
             val groupIntersection = asIterable[0].intersect(asIterable[1]).intersect(asIterable[2])
-            println("gi: $groupIntersection")
+            logger.debug("gi: $groupIntersection")
             val groupPriority = getPriority(groupIntersection.first())
-            println("gp: $groupPriority")
+            logger.debug("gp: $groupPriority")
             groupPrioritySum += groupPriority
         }
         elfNr = (elfNr + 1) % 3;
     }
 
-    println("Priority sum: $prioritySum")
-    println("Group priority sum: $groupPrioritySum")
+    logger.info("Priority sum: $prioritySum")
+    logger.info("Group priority sum: $groupPrioritySum")
 }
 
 private fun getPriority(intersection: Char): Int {
