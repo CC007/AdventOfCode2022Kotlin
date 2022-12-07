da71476c6af31f6a752c4a2bbae37032d7ef413b
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-07 21:53:19 +0100

    Day 7: implement total size

--- aoc-2022-kotlin-day7/src/main/kotlin/Path.kt
+++ aoc-2022-kotlin-day7/src/main/kotlin/Path.kt
@@ -1,7 +1,15 @@
-sealed class Path(val name: String, val parent: Dir?)
-sealed interface Dir : MutableMap<String, SubPath>
+sealed class Path(val name: String, val parent: Dir?) {
+    abstract fun getTotalSize(): Int
+}
+sealed interface Dir : MutableMap<String, SubPath> {
+    fun getTotalSize(): Int {
+        return values.map(Path::getTotalSize).sum()
+    }
+}
 
 class RootDir(private val children: MutableMap<String, SubPath>) : Path("/", null), Dir, MutableMap<String, SubPath> by children{
+    override fun getTotalSize(): Int = super.getTotalSize()
+
     override fun toString(): String {
         return "$children"
     }
@@ -9,11 +17,15 @@ class RootDir(private val children: MutableMap<String, SubPath>) : Path("/", nul
 sealed class SubPath(name: String, parent: Dir) : Path(name, parent)
 
 class SubDir(name: String, parent: Dir, private val children: MutableMap<String, SubPath>) : SubPath(name, parent), Dir, MutableMap<String, SubPath> by children {
+    override fun getTotalSize(): Int = super.getTotalSize()
+
     override fun toString(): String {
         return "$children"
     }
 }
 class File(name: String, parent: Dir, val size: Int) : SubPath(name, parent){
+    override fun getTotalSize(): Int = size
+
     override fun toString(): String {
         return "$size"
     }
