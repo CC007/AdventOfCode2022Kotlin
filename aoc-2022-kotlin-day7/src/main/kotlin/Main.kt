73c1170db12ed489301e0493782553cad0eaef34
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-07 22:14:26 +0100

    Day 7: implement visitor for printing and getting the puzzle answer

--- aoc-2022-kotlin-day7/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day7/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import mu.KotlinLogging
+import java.util.function.Consumer
+import java.util.function.Predicate
 
 private val logger = KotlinLogging.logger {}
 
@@ -11,4 +13,22 @@ fun main() {
 
     val parsed = Parser(text).parseInput()
     logger.debug(parsed.toString())
+    visit(parsed, { it is Dir }) {
+        logger.info("${it.name}: ${it.getTotalSize()}")
+    }
+
+    var totalSizeUnder100kSum = 0
+    visit(parsed, { it is Dir && it.getTotalSize() < 100_000 }) {
+        totalSizeUnder100kSum += it.getTotalSize()
+    }
+    logger.info("Total size of dirs < 100k: $totalSizeUnder100kSum")
+}
+
+fun visit(path: Path, criteria: Predicate<Path>, action: Consumer<Path>) {
+    if (criteria.test(path)) {
+        action.accept(path)
+    }
+    if (path is Dir) {
+        path.values.forEach { visit(it, criteria, action) }
+    }
 }
