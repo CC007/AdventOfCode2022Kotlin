6fbec94aa222781a883a9f2dd2e5f62d654e54c9
CC007 <Coolcat_the_best2@Hotmail.com>
2022-12-20 02:39:21 +0100

    Day 12: optimize logging some more and set input file

--- aoc-2022-kotlin-day12/src/main/kotlin/Main.kt
+++ aoc-2022-kotlin-day12/src/main/kotlin/Main.kt
@@ -2,24 +2,26 @@ import Square.Grid
 
 fun main() {
     val text = object {}.javaClass
-        .getResource("inputExample.txt")!!
+        .getResource("input.txt")!!
         .readText()
         .lines()
 
+    "Parsing file".logDebug()
     val grid = parseFile(text)
-    grid.toString()::formatPretty[2, 0].logDebug()
+    grid.toString()::formatPretty[2, 0]//.logDebug()
 
     val shortestPaths: MutableMap<Square, Int> = mutableMapOf()
     grid.forEach { shortestPaths.putAll(it.map { it to Int.MAX_VALUE }) }
-    shortestPaths.toString()::formatPretty[2, 0].logTrace()
+    shortestPaths.toString()::formatPretty[2, 0]//.logTrace()
 
+    "Calculating shortest paths".logDebug()
     var searchSpace: MutableList<Pair<Square, Int>> = arrayListOf(grid.end to 0)
     do {
         val (square, steps) = searchSpace.removeFirst()
-        square.toString().formatPretty().logTrace("Current:")
-        grid.joinToString("\n") {
+        square.toString()::formatPretty[2, 0].logTrace("Current:");
+        { grid.joinToString("\n") {
             it.map { shortestPaths[it] }.joinToString(" ") { if (it == Int.MAX_VALUE) "inf" else "%3d".format(it) }
-        }.let { "Steps: $steps, grid:\n$it" }.logTrace()
+        }.let { "Steps: $steps, grid:\n$it" } }.logTrace()
         if (steps < shortestPaths[square]!!) shortestPaths[square] = steps
         if (square.left?.canMoveRight == true && shortestPaths[square.left]!! == Int.MAX_VALUE) searchSpace.add(square.left!! to (steps + 1))
         if (square.right?.canMoveLeft == true && shortestPaths[square.right]!! == Int.MAX_VALUE) searchSpace.add(square.right!! to (steps + 1))
@@ -27,8 +29,14 @@ fun main() {
         if (square.down?.canMoveUp == true && shortestPaths[square.down]!! == Int.MAX_VALUE) searchSpace.add(square.down!! to (steps + 1))
         searchSpace = searchSpace.distinctBy { it.first }.toMutableList()
     } while (square != grid.start)
+    { grid.joinToString("\n") {
+        it.map { shortestPaths[it] }.joinToString(" ") { if (it == Int.MAX_VALUE) "inf" else "%3d".format(it) }
+    }.let { "Grid:\n$it" } }.logDebug()
 
+    //12a
     "Shortest path to end: ${shortestPaths[grid.start]}".logInfo()
+    
+    //12b
     "Shortest path to end from any a (pos ${shortestPaths.filter { it.key.elevation == 0 }.minBy { it.value }.let { "${it.key.pos}): ${it.value}"}}".logInfo()
 }
 
